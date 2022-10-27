200de9a22dfc2b91b6bc64598c0a5f2acb5d1a8f
Kitone Elvis Peter <33476085+elviskitone@users.noreply.github.com>
2022-10-27 10:44:00 +0300

    Update connection.py
    Modified MySQL db connector, replaced name and password with dummy

--- connection.py
+++ connection.py
@@ -1,19 +1,8 @@
-# import pymysql.cursors
-
-# connection = pymysql.connect(host='localhost',
-#                               user='oboth',
-#                               password='oboth',
-#                               db='wdrDb',
-#                               charset='utf8mb4',
-#                               cursorclass=pymysql.cursors.DictCursor)
-
-
-
 import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="elvis",
-  passwd="kitone",
+  user="name",
+  password="password",
   database="wdrDb"
 )
